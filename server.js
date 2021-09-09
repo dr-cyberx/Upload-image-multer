3439050d7191cda9a2ed035d5b06c93b053ae856
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-09-09 18:06:14 +0530

    changes

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ import ejs from 'ejs';
 import path from 'path';
 import multer from 'multer';
 
+// set Storage Engine
+
+const storage = multer.diskStorage({
+  destination:'./public/uploads/',
+  filename: function (req, file, cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage
+}).single('myImage');
+
 
 const app = express();
 
@@ -13,7 +26,20 @@ app.use(express.static('./public'));
 
 app.get("/", (req, res)=>{
   res.render("Index")
-})
+});
+
+app.post("/upload", (req, res)=>{
+  upload(req, res, (err)=>{
+     if(err){
+       res.render("Index", {
+         msg: err
+       })
+     }else{
+       console.log(req.file);
+       res.send('test')
+     }
+  })
+});
 
 
 app.listen(4000, ()=>{
